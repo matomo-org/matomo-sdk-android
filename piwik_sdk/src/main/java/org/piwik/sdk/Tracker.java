b0039f03af8418634669cba10be1ced003477dda
Alexey Subbotin <a.subbotin@clearcode.cc>
2014-10-08 10:37:14 +0200

    #9 licensing headers added [ci skip]

--- piwik_sdk/src/main/java/org/piwik/sdk/Tracker.java
+++ piwik_sdk/src/main/java/org/piwik/sdk/Tracker.java
@@ -1,3 +1,10 @@
+/*
+ * Android SDK for Piwik
+ *
+ * @link https://github.com/piwik/piwik-android-sdk
+ * @license https://github.com/piwik/piwik-sdk-android/blob/master/LICENSE BSD-3 Clause
+ */
+
 package org.piwik.sdk;
 
 import android.app.Activity;
