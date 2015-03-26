c0af19a23f43e8f78cbf5d750567f79cb6a1167b
darken <d4rken@gmail.com>
2015-03-26 13:06:43 +0100

    Added documentation that mentioned getTracker is not thread-safe and added a newTracker method.

--- piwik_sdk/src/main/java/org/piwik/sdk/PiwikApplication.java
+++ piwik_sdk/src/main/java/org/piwik/sdk/PiwikApplication.java
@@ -8,38 +8,64 @@
 package org.piwik.sdk;
 
 import android.app.Application;
-import android.util.Log;
 
 import java.net.MalformedURLException;
 
 public abstract class PiwikApplication extends Application {
-    Tracker piwikTracker;
+    private Tracker mPiwikTracker;
 
-    public Piwik getPiwik(){
+    public Piwik getPiwik() {
         return Piwik.getInstance(this);
     }
 
-    public Tracker getTracker() {
-        if (piwikTracker != null) {
-            return piwikTracker;
+    /**
+     * Gives you a persisted Tracker object.
+     * <p/>
+     * The returned Tracker is not threadsafe at the moment.
+     * For use in threads create yourself a new tracker, see {@link #newTracker()}
+     *
+     * @return a shared Tracker
+     */
+    public synchronized Tracker getTracker() {
+        if (mPiwikTracker == null) {
+            mPiwikTracker = newTracker();
         }
-
-        try {
-            piwikTracker = getPiwik().newTracker(getTrackerUrl(), getSiteId(), getAuthToken());
-        } catch (MalformedURLException e) {
-            Log.i(Tracker.LOGGER_TAG, getTrackerUrl());
-            Log.w(Tracker.LOGGER_TAG, "url is malformed", e);
-            return null;
-        }
-
-        return piwikTracker;
-
+        return mPiwikTracker;
     }
 
+    /**
+     * Creates a new Tracker instance.
+     *
+     * @return a new Tracker, just for you.
+     */
+    public Tracker newTracker() {
+        try {
+            return getPiwik().newTracker(getTrackerUrl(), getSiteId(), getAuthToken());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Tracker URL was malformed.");
+        }
+    }
+
+    /**
+     * The URL of your remote Piwik server.
+     *
+     * @return
+     */
     public abstract String getTrackerUrl();
 
+    /**
+     * The siteID you specified for this application in Piwik.
+     *
+     * @return
+     */
     public abstract Integer getSiteId();
 
+
+    /**
+     * @deprecated An authoken from the Piwik server that allows more advanced features.
+     * It is encouraged that you do not use this within your app as the token can't be stored securely.
+     */
     @Deprecated
     public String getAuthToken() {
         return null;
