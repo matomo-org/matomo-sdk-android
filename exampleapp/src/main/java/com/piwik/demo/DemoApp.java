e73e03cb1755fb250705363e127af1722fdd26e8
darken <darken@darken.eu>
2016-01-13 07:40:56 +0100

    Changes to reflect better best practices. * Initialising specific tracker parameters in the activity is usually too late. * Unless specifically necessary using the UUID token for user identification is better than the wifi mac. * Debug & DryRun mode depending on BuildConfig.DEBUG

--- exampleapp/src/main/java/com/piwik/demo/DemoApp.java
+++ exampleapp/src/main/java/com/piwik/demo/DemoApp.java
@@ -7,8 +7,8 @@
 
 package com.piwik.demo;
 
-import org.piwik.sdk.Piwik;
 import org.piwik.sdk.PiwikApplication;
+import org.piwik.sdk.Tracker;
 
 public class DemoApp extends PiwikApplication {
 
@@ -24,7 +24,28 @@ public class DemoApp extends PiwikApplication {
 
     @Override
     public void onCreate() {
-        Piwik.getInstance(this).setDebug(true);
         super.onCreate();
+        initPiwik();
+    }
+
+
+    private void initPiwik() {
+        // Print debug output when working on an app.
+        getPiwik().setDebug(BuildConfig.DEBUG);
+
+        // When working on an app we don't want to skew tracking results.
+        getPiwik().setDryRun(BuildConfig.DEBUG);
+
+        // If you want to set a specific userID other than the random UUID token, do it NOW to ensure all future actions use that token.
+        // Changing it later will track new events as belonging to a different user.
+        // String userEmail = ....preferences....getString
+        // getTracker().setUserId(userEmail);
+
+        // Track this app install, this will only trigger once per app version.
+        // i.e. "http://com.piwik.demo:1/185DECB5CFE28FDB2F45887022D668B4"
+        getTracker().trackAppDownload(this, Tracker.ExtraIdentifier.APK_CHECKSUM);
+        // Alternative:
+        // i.e. "http://com.piwik.demo:1/com.android.vending"
+        // getTracker().trackAppDownload();
     }
 }
