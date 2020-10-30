f50169d13f979a89fcea97b3b792856dd61fff71
isaacwhyuenac <whyuenac@gmail.com>
2020-10-30 16:59:45 +0800

    update DATETIME_OF_REQUEST to unix timestamp

--- tracker/src/main/java/org/matomo/sdk/Tracker.java
+++ tracker/src/main/java/org/matomo/sdk/Tracker.java
@@ -16,7 +16,7 @@ import org.matomo.sdk.dispatcher.Dispatcher;
 import org.matomo.sdk.dispatcher.Packet;
 import org.matomo.sdk.tools.Objects;
 
-import java.text.SimpleDateFormat;
+//import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -415,7 +415,7 @@ public class Tracker {
         trackMe.trySet(QueryParams.RECORD, DEFAULT_RECORD_VALUE);
         trackMe.trySet(QueryParams.API_VERSION, DEFAULT_API_VERSION_VALUE);
         trackMe.trySet(QueryParams.RANDOM_NUMBER, mRandomAntiCachingValue.nextInt(100000));
-        trackMe.trySet(QueryParams.DATETIME_OF_REQUEST, new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ", Locale.US).format(new Date()));
+        trackMe.trySet(QueryParams.DATETIME_OF_REQUEST, System.currentTimeMillis());
         trackMe.trySet(QueryParams.SEND_IMAGE, "0");
 
         trackMe.trySet(QueryParams.VISITOR_ID, mDefaultTrackMe.get(QueryParams.VISITOR_ID));
